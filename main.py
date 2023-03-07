2bf63e6885bed9023f62104e8deef607669c291e
rodriguezryan1 <rodriguezryan@ufl.edu>
2023-03-07 09:25:42 -0500

    added encoder function

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
 # This lab was pair programmed by Ryan and Day on 3/7/23
+
+def encoder(password):
+    encoded = ""
+    for char in password:
+        encoded += str(int(char) + 3)
+    return encoded
